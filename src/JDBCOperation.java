37e67ab8b86e63c220a98c0b6d3b2707e8ac3ed0
zyglitch <1423845155@qq.com>
2025-07-25 16:54:30 +0800

    重命名测试方法为getAlldata，以更清晰地反映其功能

--- src/JDBCOperation.java
+++ src/JDBCOperation.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class JDBCOperation {
 
     @Test
-    public void test() throws Exception {
+    public void getAlldata () throws Exception {
         // 1. 注册驱动
         Class.forName("com.mysql.jdbc.Driver");
 
