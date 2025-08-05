3ab6fa63b6ee768b84ef3c7137d4107deb64b699
zyglitch <1423845155@qq.com>
2025-08-05 17:29:21 +0800

    JDBCAdvance.java添加主键返回功能并优化代码结构清理中的多余空行，

--- src/JDBCPrepared.java
+++ src/JDBCPrepared.java
@@ -15,8 +15,6 @@ import java.sql.SQLException;
 public class JDBCPrepared {
     
 
-
-
     public static void main(String[] args) throws Exception {
         //1.注册驱动
         try{
--- src/advance/JDBCAdvance.java
+++ src/advance/JDBCAdvance.java
@@ -19,7 +19,6 @@ import java.sql.SQLException;
 
 //使用ORM的实现来封装对象和集合
 
-
 public class JDBCAdvance {
 
     // 定义一个内部类来封装员工记录
@@ -35,11 +34,15 @@ public class JDBCAdvance {
             this.salary = salary;
             this.age = age;
         }
-
+        // 添加setter方法
+        public void setId(int id) {
+            this.id = id;
+        }
                 // 添加getter方法
         public int getId() {
             return id;
         }
+        
 
         public String getName() {
             return name;
@@ -102,20 +105,22 @@ public class JDBCAdvance {
             }
     }
 
-
     @Test 
-    //测试返回主键
-    public void testreturnPK(){
+    //测试返回主键回显
+    //主要就是实现了添加了一个员工之后，可以知道这个添加的员工的ID在数据库中是多少，方便做一些操作
+    public void testreturnPK() throws Exception{
         //1.获取链接
+        Connection conn = null;
         try{
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","123456");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","123456");
         }catch(SQLException e){
             System.out.println("获取链接失败！");
             e.printStackTrace();
         }
-        //2.预编译语句
+
+        //2.预编译语句,并指定返回主键
         String sql = "INSERT INTO staff (name_Staff, salary_Staff, age_Staff) VALUES (?, ?, ?)";
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
         //3.建立一个对象来存储数据，然后用这个对象来设置参数
         personRecord newPerson = new personRecord(0, "张强", 5000.00, 30);
@@ -123,14 +128,33 @@ public class JDBCAdvance {
         preparedStatement.setDouble(2, newPerson.getSalary());
         preparedStatement.setInt(3, newPerson.getAge());
 
+        //4.执行SQL语句，并返回主键
+        int rowsAffected = preparedStatement.executeUpdate();
+
+        //5.处理结果
+        if(rowsAffected > 0){
+            System.out.println("收到数据");
+
+            //这个返回的是一个结果集，放的是单行单列
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if(generatedKeys.next()){
+                int newId = generatedKeys.getInt(1);//获取新插入的记录的ID
+                newPerson.setId(newId);//更新对象的ID属性
+                System.out.println("员工添加成功，员工ID为：" + newId);
+            }else{
+                System.out.println("员工添加成功，但无法获取员工ID");
+            }
+        }
+
+        //6.关闭资源
+        preparedStatement.close();
+        conn.close();
         
+
+
+
+
     }
-
-
-
-
-
-
 
 }
 
